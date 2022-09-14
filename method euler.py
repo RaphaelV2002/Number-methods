719530633432b1858c878b0c6fd010878d3d1964
RaphaelV2002 <108413879+RaphaelV2002@users.noreply.github.com>
2022-09-14 12:21:17 +0400

    Update method euler.py

--- method euler.py
+++ method euler.py
@@ -4,13 +4,17 @@ import numpy as np
 def Euler(a, b, h, x, u):
     print('x', 'u', 'y-u')
     n = int((b-a)/h)
-    um = np.zeros(n)  # Массив численных решений
+    um = np.zeros(n)# Массив численных решений
+    err = np.zeros(n)
     for i in range(n):
         u += h * fun(x, u)  # Численное решение
         y = -1.0/(-x-1.0)  # Аналитическое решение
-        print(round(x, 3), round(u, 3), round(y-u, 3))
+        err[i]=abs(y-u)
+        print(round(x, 3), round(u, 3), round(err[i], 3))
         x += h
         um[i] = u
+    print(max(err))
+
     return um
 
 
