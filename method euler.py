8fdf9bf751e8f5d5a1f7634139900198abd508e8
RaphaelV2002 <108413879+RaphaelV2002@users.noreply.github.com>
2022-09-14 13:27:11 +0400

    Update method euler.py

--- method euler.py
+++ method euler.py
@@ -1,21 +1,32 @@
+import matplotlib.pyplot as plt
+from numpy import arange
+from numpy import meshgrid
 import numpy as np
-
-
 def Euler(a, b, h, x, u):
     print('x', 'u', 'y-u')
     n = int((b-a)/h)
-    um = np.zeros(n)# Массив численных решений
-    err = np.zeros(n)
+    u = np.zeros(n+1)# Массив численных решений
+    err = np.zeros(n+1)
+    y = np.zeros(n+1)
     for i in range(n):
-        u += h * fun(x, u)  # Численное решение
-        y = -1.0/(-x-1.0)  # Аналитическое решение
-        err[i]=abs(y-u)
-        print(round(x, 3), round(u, 3), round(err[i], 3))
+        u[i+1] = u[i] + h * fun(x+(h/2.0), u[i]+(h/2.0)*fun(x,u[i]))  # Численное решение
+        y[i+1] = -1.0/(-x-1.0)  # Аналитическое решение
+        err[i]=abs(y[i+1]-u[i+1])
+        print(round(x, 3), round(u[i+1], 3), round(err[i], 3))
         x += h
-        um[i] = u
     print(max(err))
-
-    return um
+    delta = 0.025
+    xrange = arange(-100.0, 100.0, delta)
+    yrange = arange(-100.0, 100.0, delta)
+    x, y = meshgrid(xrange,yrange)
+    plt.axhline(y=0, color='k')
+    plt.axvline(x=0, color='k')
+    plt.plot(x,u*100.0,"ro")
+    plt.plot(x,y*100.0,"bo")
+    plt.plot(x,err*100.0,"go")
+    plt.grid()
+    plt.show()
+    return u
 
 
 def fun(x, u):
