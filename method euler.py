b4b783b54071e7f96b4bc8b623c5f6a092e100ee
RaphaelV2002 <108413879+RaphaelV2002@users.noreply.github.com>
2022-09-15 23:27:39 +0400

    Update method euler.py

--- method euler.py
+++ method euler.py
@@ -1,20 +1,27 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def Euler(a, b, h, x, u):
     print('x', 'u', 'y-u')
     n = int((b-a)/h)
-    um = np.zeros(n)# Массив численных решений
+    um = np.zeros(n)  # Массив численных решений
+    ym = np.zeros(n)
+    xm = np.zeros(n)
     err = np.zeros(n)
     for i in range(n):
-        u += h * fun(x, u)  # Численное решение
-        y = -1.0/(-x-1.0)  # Аналитическое решение
-        err[i]=abs(y-u)
-        print(round(x, 3), round(u, 3), round(err[i], 3))
-        x += h
+        xm[i] = x
         um[i] = u
+        u += h * fun(x, u+(h/2.0)*fun(x, u))  # Численное решение
+        y = -1.0/(-x-1.0)  # Аналитическое решение
+        ym[i] = y
+        err[i] = abs(y-u)
+        x += h
+        print(round(x, 3), round(u, 3), round(err[i], 3))
     print(max(err))
-
+    plt.plot(xm, um)
+    plt.plot(xm, err*100)
+    plt.show()
     return um
 
 
